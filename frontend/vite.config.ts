59c3007074c0c0db7fcc8aca6b3deba4fe092a4e
pkolenic <pkolenic@gmail.com>
2025-11-21 11:19:08 -0700

    Fix setting up the API proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,29 +1,40 @@
 import react from "@vitejs/plugin-react"
 import * as path from "node:path"
-import { defineConfig } from "vitest/config"
-import packageJson from "./package.json" with { type: "json" }
+import {defineConfig} from "vitest/config"
+import packageJson from "./package.json" with {type: "json"}
+
+// Define a default port in case the environment variable isn't set
+const backendPort = process.env.VITE_BACKEND_PORT || 3001;
+const targetUrl = `http://localhost:${backendPort}`;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
 
-  server: {
-    open: true,
-    port: parseInt(process.env.VITE_FRONTEND_PORT || '5173', 10),
-  },
-
-  test: {
-    root: import.meta.dirname,
-    name: packageJson.name,
-    environment: "jsdom",
-
-    typecheck: {
-      enabled: true,
-      tsconfig: path.join(import.meta.dirname, "tsconfig.json"),
+    server: {
+        open: true,
+        port: parseInt(process.env.VITE_FRONTEND_PORT || '5173', 10),
+        // Proxy Settings so that the frontend can make requests to the backend
+        proxy: {
+            "/api": {
+                target: targetUrl,
+                changeOrigin: true,
+            },
+        },
     },
 
-    globals: true,
-    watch: false,
-    setupFiles: ["./src/setupTests.ts"],
-  },
+    test: {
+        root: import.meta.dirname,
+        name: packageJson.name,
+        environment: "jsdom",
+
+        typecheck: {
+            enabled: true,
+            tsconfig: path.join(import.meta.dirname, "tsconfig.json"),
+        },
+
+        globals: true,
+        watch: false,
+        setupFiles: ["./src/setupTests.ts"],
+    },
 })
