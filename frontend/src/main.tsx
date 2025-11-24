25107da372d96b38b5c51f4ded2dd8004b51c2a5
pkolenic <pkolenic@gmail.com>
2025-11-24 11:47:39 -0700

    Refactor audience env variable name

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -18,7 +18,7 @@ if (container) {
                 clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
                 authorizationParams={{
                     redirect_uri: window.location.origin,
-                    audience: import.meta.env.VITE_AUTH0_API_IDENTIFIER,
+                    audience: import.meta.env.VITE_AUTH0_AUDIENCE,
                 }}
             >
                 <Provider store={store}>
