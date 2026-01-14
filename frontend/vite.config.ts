5408fe798df83b4e96e99b75f73bb3997f6f162c
pkolenic <pkolenic@gmail.com>
2026-01-13 18:06:17 -0700

    Add support for running with local SSL for local testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import react from "@vitejs/plugin-react"
-import * as path from "node:path"
-import {defineConfig} from "vitest/config"
-import packageJson from "./package.json" with {type: "json"}
+import react from "@vitejs/plugin-react";
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import * as path from "node:path";
+import {defineConfig} from "vitest/config";
+import packageJson from "./package.json" with {type: "json"};
 
 // Define a default port in case the environment variable isn't set
 const backendPort = process.env.VITE_BACKEND_PORT || 3001;
@@ -9,11 +10,15 @@ const targetUrl = `http://localhost:${backendPort}`;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+      react(),
+      basicSsl(),
+    ],
 
     server: {
         open: true,
         port: parseInt(process.env.VITE_FRONTEND_PORT || '5173', 10),
+        host: true,
         // Proxy Settings so that the frontend can make requests to the backend
         proxy: {
             "/api": {
