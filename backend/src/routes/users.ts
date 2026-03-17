92eee4941ea4e198e3fe8608ce7634ee15b8657b
pkolenic <pkolenic@gmail.com>
2026-03-17 13:18:59 -0600

    Cleanup File layout and use mountRoute helper to mount addressRouter

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -17,6 +17,7 @@ import {
   updateUserRules,
   updatePasswordRules,
 } from "../middleware/index.js";
+import { mountRoute } from '../utils/routes.js';
 
 //Define allowed permission strings
 type UserPermission =
@@ -38,23 +39,29 @@ const router: Router = Router();
 // USER ROUTES
 router.get('/', guard('read:users'), getUsers);
 router.post('/', guard('create:users', createUserRules), createUser);
+router.post('/sync', syncUser);
+
 router.route('/:userId')
   .get(guardedResource('read:users', [], true), getUser)
   .delete(guardedResource('delete:users'), deleteUser)
   .put(guardedResource('update:users', updateUserRules), updateUser)
+
 router.put('/:userId/password',
   guardedResource('self', updatePasswordRules, true),
   updateUserPassword,
 );
-router.post('/sync', syncUser);
 
 // ADDRESS ROUTES
 const addressRouter = Router({ mergeParams: true });
+
 addressRouter.post('/', guardedResource('create:addresses', [], true), createAddress);
+
 addressRouter.route('/:addressId')
   .put(guardedResource('update:addresses', [], true), updateAddress)
   .delete(guardedResource('delete:addresses', [], true), deleteAddress);
-router.use('/:userId/address', addressRouter);
+
+// use mountRoute to mount the addressRouter under /:userId/address and include the mountPath on the routes for testing
+mountRoute(router, '/:userId/address', addressRouter);
 
 // Export the router
 export default router;
