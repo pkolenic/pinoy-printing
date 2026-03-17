72366f041e2bf5ea60e38ff0105ff38b71bad8e3
pkolenic <pkolenic@gmail.com>
2026-03-16 18:43:50 -0600

    Add User Validation Tests and Refactor Rules

--- backend/src/middleware/validations/users.ts
+++ backend/src/middleware/validations/users.ts
@@ -2,19 +2,24 @@ import { body } from 'express-validator';
 import { withValidation } from "./common.js";
 
 // Shared Base Rules
-const emailBase = body('email')
+const getEmailBase = () => body('email')
+  .exists().withMessage('Email is required')
+  .bail()
   .isEmail().withMessage('Please provide a valid email address')
   .normalizeEmail(); // Sanitizes email (e.g., lowercase, removes dots in Gmail)
 
 // Core phone validation logic
-const phoneBase = body('phone')
+const getPhoneBase = () => body('phone')
+  .optional({ values: 'null' })
   .isMobilePhone('any', { strictMode: true })
   .withMessage('Invalid phone format, use the E.164 format ^+[0-9]{1,15}$');
 
 // Core password validation logic
-const passwordBase = body('password')
-  .isLength({ min: 8 })
-  .withMessage('Password must be at least 8 characters long')
+const getPasswordBase = () => body('password')
+  .exists().withMessage('Password is required')
+  .bail()
+  .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+  .bail()
   .isStrongPassword({
     minLength: 8,
     minLowercase: 1,
@@ -22,31 +27,29 @@ const passwordBase = body('password')
     minNumbers: 1,
     minSymbols: 1,
   })
-  .withMessage(
-    'Password must contain at least 8 characters, including uppercase, lowercase, numbers, and special characters'
-  );
+  .withMessage('Password must include uppercase, lowercase, numbers, and symbols');
 
 /**
  * Rules for POST /api/users/
  */
 export const createUserRules = withValidation([
-  emailBase,
-  phoneBase.optional(),
-  passwordBase,
+  getEmailBase(),
+  getPhoneBase(),
+  getPasswordBase(),
 ]);
 
 /**
  * Rules for PUT /api/users/:userId
  */
 export const updateUserRules = withValidation([
-  emailBase.optional(),
-  phoneBase.optional(),
+  getEmailBase().optional(),
+  getPhoneBase(),
 ]);
 
 /**
  * Rules for PUT /api/users/:userId/password
  */
 export const updatePasswordRules = withValidation([
-  passwordBase,
+  getPasswordBase(),
 ]);
 
