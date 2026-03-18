f16f243754c137a43c213f689c29c1ebaffc0c70
pkolenic <pkolenic@gmail.com>
2026-03-17 22:09:17 -0600

    Add updateOrderRules validation, add addresses to base validation

--- backend/src/middleware/validations/orders.ts
+++ backend/src/middleware/validations/orders.ts
@@ -29,8 +29,48 @@ const getOrderBase = () => [
   body('items.*.customization')
     .optional()
     .isObject().withMessage('Each item\'s customization must be an object'),
+
+  // TODO - how to validate address is setup for the country(ies) that the site is configured for?
+
+  body('address')
+    .optional()
+    .isObject().withMessage('Shipping address must be an object'),
+
+  body('address.street')
+    .if(body('address').exists())
+    .notEmpty().withMessage('Street is required'),
+
+  body('address.city')
+    .if(body('address').exists())
+    .notEmpty().withMessage('City is required'),
+
+  body('address.region')
+    .if(body('address').exists())
+    .notEmpty().withMessage('Region is required'), // TODO - need message based on site configuration, need to validate region codes based on site configuration
+
+  body('address.postalCode')
+    .if(body('address').exists())
+    .notEmpty().withMessage('Postal code is required'),  // TODO - need message based on site configuration, need to validate postal codes based on site configuration
+
+  body('address._isPrimaryInput').not().exists().withMessage('Orders do not support multiple addresses'),
 ]
 
 export const createOrderRules = withValidation([
   ...getOrderBase(),
+]);
+
+export const updateOrderRules = withValidation([
+  ...getOrderBase().map(rule => rule.optional()),
+
+  // Prevent clients from manually setting the price during an update
+  body('items.*.price')
+    .not().exists()
+    .withMessage('Item prices are calculated by the system and cannot be modified directly'),
+
+  // Prevent changing the User the order belongs to
+  body('userId').not().exists().withMessage('The order owner cannot be changed'),
+
+  // Explicitly block status/paid/shipped from this general update route
+  body(['status', 'paid', 'shipped']).not().exists()
+    .withMessage('Use dedicated routes to update status, payment, or shipping info'),
 ]);
